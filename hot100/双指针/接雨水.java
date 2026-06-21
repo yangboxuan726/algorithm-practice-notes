7a441bd2574aba9a3b894f7bbbb1e7f4e439ae4d
yangboxuan <3046829330@qq.com>
2026-06-21 21:54:03 +0800

    接雨水

--- hot100/双指针/接雨水.java
+++ hot100/双指针/接雨水.java
@@ -1,26 +1,27 @@
 public class 接雨水 {
     //https://leetcode.cn/problems/trapping-rain-water/description/?envType=study-plan-v2&envId=top-100-liked
-    public static void main(String[] args){
-        int[] arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
         int result = trap(arr);
         System.out.println(result);
 
     }
-    public static int trap(int[] height){
-        int l=1,r = height.length-2,lmax = height[0],rmax = height[height.length -1];
-        int ans = 0;
-        while(l <= r){
-            if (lmax < rmax){
-                ans += Math.max(0,lmax-height[l]);
-                lmax = Math.max(lmax,height[l++]);
 
-            }else {
-                ans += Math.max(0,rmax-height[r]);
-                rmax = Math.max(rmax,height[r--]);
+    public static int trap(int[] height) {
+        int l = 1, r = height.length - 2, lmax = height[0], rmax = height[height.length - 1];
+        int ans = 0;
+        while (l <= r) {
+            if (lmax < rmax) {
+                ans += Math.max(0, lmax - height[l]);
+                lmax = Math.max(lmax, height[l++]);
+
+            } else {
+                ans += Math.max(0, rmax - height[r]);
+                rmax = Math.max(rmax, height[r--]);
             }
 
         }
-        return  ans;
+        return ans;
     }
 
 }
