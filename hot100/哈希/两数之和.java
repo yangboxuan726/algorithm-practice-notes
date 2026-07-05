eeda5f2483bafdd8a6bb606eeaa67eba55c5803b
yangboxuan <3046829330@qq.com>
2026-07-05 23:55:28 +0800

    两数之和

--- hot100/哈希/两数之和.java
+++ hot100/哈希/两数之和.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class 两数之和 {
-    public int[] twoSum(int[] nums, int target) {
+    //https://leetcode.cn/problems/two-sum/description/?envType=study-plan-v2&envId=top-100-liked
+    public static void main(String[] args) {
+        int target = 9;
+        int[] nums = {2,7,11,15};
+        int[] sum = twoSum(nums, target);
+        System.out.println(Arrays.toString(sum));
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
         for(int i = 0; ; i++){
             for(int j = i + 1;j < nums.length;j++){
                 if(nums[i] + nums[j] == target){
