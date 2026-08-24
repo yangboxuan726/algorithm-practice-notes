c6938b267727f1c3e05c9505d414638132a2cc4e
ybx <3046829330@qq.com>
2026-08-24 20:49:22 +0800

    对称二叉树

---
+++ hot100/二叉树/对称二叉树.java
@@ -0,0 +1,43 @@
+public class 对称二叉树 {
+    //https://leetcode.cn/problems/symmetric-tree/?envType=study-plan-v2&envId=top-100-liked
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root){
+        if(root == null){
+            return  true;
+        }
+        return dfs(root.left,root.right);
+    }
+
+    public  boolean dfs(TreeNode left, TreeNode right){
+        if (left == null && right == null){
+            return true;
+        }
+        if (left == null || right == null){
+            return false;
+        }
+
+        if (left.val != right.val){
+            return false;
+        }
+
+        return dfs(left.left,right.right) && dfs(left.right,right.left);
+    }
+}
